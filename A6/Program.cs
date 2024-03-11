0627f4d1b9caf495be9c11a475405985a709d18d
Aryan Sabet Nejad <74782534+aryansabet@users.noreply.github.com>
2024-03-11 20:51:58 +0330

    chore: added templates

---
+++ A6/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A6;
+
+public class Program
+{
+    static void Main(string[] args)
+    { }
+    public static int GetObjectType(object o)
+    {
+        throw new NotImplementedException();
+    }
+
+    public static bool IdealHusband(FutureHusbandType fht)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ test.bat
@@ -0,0 +1,22 @@
+@ECHO OFF
+
+dotnet test --filter "FullyQualifiedName=A6.Tests.MemoryTestTests.VariableSizeTest"
+
+dotnet test --filter "FullyQualifiedName=A6.Tests.MemoryTestTests.StackDepth10Test"
+dotnet test --filter "FullyQualifiedName=A6.Tests.MemoryTestTests.StackDepth100Test"
+dotnet test --filter "FullyQualifiedName=A6.Tests.MemoryTestTests.StackDepth1000Test"
+dotnet test --filter "FullyQualifiedName=A6.Tests.MemoryTestTests.StackDepth3000Test"
+
+dotnet test --filter "FullyQualifiedName=A6.Tests.MemoryTestTests.HeapMemoryTest"
+
+dotnet test --filter "FullyQualifiedName=A6.Tests.MemoryTestTests.RefValueTypeCopyTest1"
+dotnet test --filter "FullyQualifiedName=A6.Tests.MemoryTestTests.RefValueTypeCopyTest2"
+dotnet test --filter "FullyQualifiedName=A6.Tests.MemoryTestTests.RefValueTypeCopyTest3"
+
+dotnet test --filter "FullyQualifiedName=A6.Tests.MemoryTestTests.BoxingTest"
+
+dotnet test --filter "FullyQualifiedName=A6.Tests.MemoryTestTests.TypeTest"
+
+dotnet test --filter "FullyQualifiedName=A6.Tests.MemoryTestTests.IdealHusbandTest"
+
+ECHO All tests completed!
